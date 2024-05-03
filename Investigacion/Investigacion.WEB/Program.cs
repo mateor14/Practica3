f26aff25c4506cd598bb4db59eef838606beabdb
MADRIGAL\huert <huertasf3010@gmail.com>
2024-05-03 17:19:04 -0500

    Commit Part 2 WEB

--- Investigacion/Investigacion.WEB/Program.cs
+++ Investigacion/Investigacion.WEB/Program.cs
@@ -1,4 +1,6 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Investigacion.WEB;
+using Investigacion.WEB.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7043/") });
+builder.Services.AddSweetAlert2();
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
